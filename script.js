34d5b94ddc7099f31c1700c7871ab35da67e7b4e
cyber-space11 <cyberspaceweb11@gmail.com>
2026-06-05 05:19:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+ const messages = [
+    "Are you sure?",
+    "Really sure??",
+    "Are you positive?",
+    "Pookie please...",
+    "Just think about it!",
+    "If you say no, I will be really sad...",
+    "I will be very sad...",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    
+    // تغيير نص زر الـ No
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    
+    // تكبير زر الـ Yes بطريقة متجاوبة وآمنة للشاشات
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.4}px`; // تكبير بنسبة 1.4 ليعطي فرصة أكبر لظهور النص
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
