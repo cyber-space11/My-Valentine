97a3c978a7cdd110450a79c0ced0b1743b829003
cyber-space11 <cyberspaceweb11@gmail.com>
2026-06-05 05:35:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
  const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "أحجي الصدك?",
+    "لا لا بدون شقة ??",
+    "لتجذب?",
+    "متأكد ... ؟",
+    "فكر بعد شوية !",
+    "ترة اذا تكول لا  رح ازعل ؟...",
+    "ترة ازعل من صدك ... ؟",
+    "ترة كلش كلش رح ازعل ...",
+    "عادي براحتك اصلا مراح اسألك ...",
+    "بس كول أي شبيك ترة مراح اعضك ! ❤️"
 ];
 
 let messageIndex = 0;
